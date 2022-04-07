3351631ee8c4c26282da3ac423c3cd80a8f0d219
Pamela <pamelalondero@gmail.com>
2022-04-07 10:52:03 -0300

    Initial commit

---
+++ src/test/java/com/alkemi/icons/icons/IconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemi.icons.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
